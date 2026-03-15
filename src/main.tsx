e5c28484b256951a1a9d13b607aa2930cb8e4afd
r00txpwn <wilsonsam12345@gmail.com>
2026-03-15 19:45:09 +0400

    Fix main.tsx to render App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import SuppliersPage from './pages/admin/SuppliersPage';
+import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SuppliersPage />
+    <App />
   </React.StrictMode>,
 );
